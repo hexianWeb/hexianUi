97f56012b64fc4ee8dfce864ef3fe33a3a981189
hexianWeb <2387213640@qq.com>
2023-09-04 11:26:18 +0800

    :sparkles: feat(custom): 初始化组件库仓库

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import Button from "./components/Button/Button.vue";
+import type { ButtonInstance } from "./components/Button/types";
+const buttonRef = ref<ButtonInstance | null>(null);
+onMounted(() => {
+  console.log(buttonRef.value && buttonRef.value.ref);
+});
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+
+  <main>
+    <Button ref="buttonRef">Test Button</Button>
+    <Button plain>Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>VK</Button>
+    <Button disabled>Disabled Button</Button><br /><br />
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br /><br />
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br /><br />
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button><br /><br />
+  </main>
+</template>
