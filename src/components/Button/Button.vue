bb09e87b55b33b469e041f3c34877fe9139c98c3
hexianWeb <2387213640@qq.com>
2023-09-04 11:27:07 +0800

    :sparkles: feat(custom): 新增 hexian ui =\> button 组件

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    ref="_ref"
+    class="hx-button"
+    :class="{
+      [`hx-button--${type}`]: type,
+      [`hx-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span><slot></slot></span>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./types";
+defineOptions({
+  name: "hxButton",
+  inheritAttrs: false,
+});
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: "button",
+});
+
+const _ref = ref<HTMLButtonElement>();
+
+defineExpose({
+  ref: _ref,
+});
+</script>
+
+<style></style>
